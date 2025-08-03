a30b57d8eb48c4c20abf0ca301d91f94ad2708d6
Abdullah Rashid <89905186+AbdullahRashid133@users.noreply.github.com>
2025-08-03 14:21:45 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/coderealm/main.go
@@ -0,0 +1,23 @@
+// cmd/coderealm/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"coderealm/internal/coderealm"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := coderealm.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
